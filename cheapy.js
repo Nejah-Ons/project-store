dc62d1e6f7dd05c452d5be12cedf73fcbf0a6c50
nejah2essafi <nejah.essafi@hotmail.fr>
2019-11-05 18:59:30 +0100

    progress

--- cheapy.js
+++ cheapy.js
@@ -16,11 +16,16 @@ var total_prix =0 ;
           document.getElementById(id_item).value= 0;
 
 			}
+
+
+			
 				
 	function total_price(){
 
 		
-		 total_prix =( document.getElementById("dresse-2").value*30) + ( document.getElementById("dresse-1").value*60)+ ( document.getElementById("suit-1").value*90)+ ( document.getElementById("suit-2").value*80);
+		 total_prix =( document.getElementById("dresse-2").value*30) + ( document.getElementById("dresse-1").value*60)+ ( document.getElementById("suit-1").value*90)
+		 + ( document.getElementById("suit-2").value*80)+( document.getElementById("dresse-3").value*70)+( document.getElementById("dresse-4").value*65)
+		 + ( document.getElementById("suit-3").value*95)+ ( document.getElementById("suit-4").value*85);
 		$("#total").html(total_prix);
  
 	}
@@ -28,6 +33,12 @@ var total_prix =0 ;
 
     $("#section_dresse-1").hide();
     $("#section_dresse-2").hide();
+      $("#section_dresse-3").hide();
+       $("#section_dresse-4").hide();
     $("#section_suit-1").hide();
     $("#section_suit-2").hide();
+    $("#section_suit-3").hide();
+    $("#section_suit-4").hide();
+    
+				
 });
